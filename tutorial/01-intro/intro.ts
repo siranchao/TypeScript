ade91fe5fc16476e295a961c9578eb74ecb35901
Siran Cao <siranchao@gmail.com>
2023-05-03 23:25:09 -0400

    first init

---
+++ tutorial/01-intro/intro.ts
@@ -0,0 +1,12 @@
+
+//typescript primitive types
+const userName: string = 'Hello World!';
+
+const userId: number = 20;
+
+//convert to string
+const sales: number = 20.21;
+const netSales: string = sales.toFixed();
+
+const isLoggedIn: boolean = true;
+
